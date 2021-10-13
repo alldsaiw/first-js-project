e8fe5aaa3cc6f58f0787fed3f70bc2d5607856d3
E. Afanaseva <milianalynx@gmail.com>
2021-10-13 12:00:37 +0300

    added code for personal movie db

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = ('На сколько его оцените?', '');
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
